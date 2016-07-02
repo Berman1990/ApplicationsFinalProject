77d8f1b34768a0c7c03abc11d6004f721c74d1eb
MatanBobi <Tcyhjtsuo8>
2016-07-02 14:03:17 +0300

    Fix client routing

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,10 @@
 /**
  * Created by matan on 01-Jul-16.
  */
-var app = angular.module( 'moviesStoreApp',
-    ['ngRoute','ngMaterial', 'uiRouterApp', 'ChatService','computerService', 'btford.socket-io'] );
+var moviesStoreApp = angular.module( 'moviesStoreApp',
+    [
+        'uiRouterApp',
+        'ngAnimate',
+        'ngResource',
+        'ngMaterial'
+    ] );
